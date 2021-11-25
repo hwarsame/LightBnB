a301a7e3b522395e0c125c763c74996eb728190c
Hamza Warsame <hamza360123@gmail.com>
2021-11-25 17:50:28 -0500

    complete queries for users

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -16,16 +16,29 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  // let user;
+  // for (const userId in users) {
+  //   user = users[userId];
+  //   if (user.email.toLowerCase() === email.toLowerCase()) {
+  //     break;
+  //   } else {
+  //     user = null;
+  //   }
+  // }
+  // return Promise.resolve(user);
+  return pool
+    .query(`SELECT * FROM users WHERE email = $1`, [email.toLowerCase()])
+    .then((result) => {
+      const user = result.rows[0]
+      if (user){
+        return (user)
+      }
+        return(null)
+        })
+        .catch((err) => {
+      console.log(err.message);
+    });
+
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -35,7 +48,19 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1`, [id])
+    .then((result) => {
+      const user = result.rows[0]
+      if (user) {
+        return (user)
+      }
+        return(null)
+        })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
 }
 exports.getUserWithId = getUserWithId;
 
@@ -46,10 +71,12 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+   .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email.toLowerCase(), user.password])
+   .then((result) => result.rows[0])
+   .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.addUser = addUser;
 
