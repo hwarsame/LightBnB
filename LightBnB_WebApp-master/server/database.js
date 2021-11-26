ca01b28b357059dfb1b3a7148fb632fe61030593
Hamza Warsame <hamza360123@gmail.com>
2021-11-25 19:55:42 -0500

    add reservations for guest_id

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -88,7 +88,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  // return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT  * 
+            FROM reservations 
+            JOIN properties ON property_id = properties.id 
+            WHERE reservations.guest_id = $1
+            AND reservations.start_date > now()::date
+            ORDER BY reservations.start_date
+            LIMIT $2`, [guest_id, limit])
+    .then((result) => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
